1673ceec83578487b3244cc711c3989362396d05
andybi7676 <b07502072@ntu.edu.tw>
2021-03-16 11:42:59 +0800

    first run

--- script/train.sh
+++ script/train.sh
@@ -5,7 +5,7 @@
 
 CONFIG="dlhlp_asr"
 
-DIR="/data/storage/harry/E2E_ASR"
+DIR="/work/b07502172/hw1-1/E2E_ASR"
 
 echo "Start running training process of E2E ASR"
 CUDA_VISIBLE_DEVICES=$2 python3 main.py --config config/${CONFIG}.yaml \
